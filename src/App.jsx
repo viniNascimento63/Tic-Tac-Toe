f77209b718145a8877e07cc927d3fa4b65814c66
Vinícius Nascimento <vini_sn@outlook.com>
2024-03-26 13:38:42 -0300

    projeto feito

---
+++ src/App.jsx
@@ -0,0 +1,139 @@
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) {
+  // Manipula os quadrados toda vez que um quadrado é clicado
+  function handleClick(i) {
+    // Verifica se existe um vencedor OU se o quadrado já está preenchido
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    // "nextSquares" é uma cópia do array squares
+    const nextSquares = squares.slice();
+
+    // Verifica a próxima jogada é de X ou de O
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
+export default function Game() {
+  // "xIsNext" representa o próximo a jogar: X ou O
+  const [xIsNext, setXIsNext] = useState(true);
+
+  // "history" representa todos as alterações do tabuleiro
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+
+  // mantem o controle de qual etapa o usuário está vizualizando
+  const [currentMove, setCurrentMove] = useState(0);
+
+  // "current" representa o último array de squares do histórico de jogadas
+  const currentSquares = history[currentMove];
+
+  // "handlePlay" será chamada pelo Board (tabuleiro) para atualizar o jogo
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  // Renderiza o tabuleiro e o histórico
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  // Possibilidades de vitória
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    // A cada iteração const [a, b, c] recebe
+    // uma das possíveis combinações de vitória.
+    const [a, b, c] = lines[i];
+
+    // Verifica se as jogadas correspondem a um combinação de vitória
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
